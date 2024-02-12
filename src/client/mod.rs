ec5300fe58cf8a5391aece8558e0bc32fa05f080
LoveSy <shana@zju.edu.cn>
2024-02-13 03:05:15 +0800

    Support bind device or address

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -8,6 +8,7 @@ use std::io;
 use std::mem::MaybeUninit;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::num::NonZeroU32;
+use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 use std::task::Poll;
 use std::{fmt::Debug, num::NonZeroU16, pin::Pin};
@@ -25,6 +26,7 @@ use url::Url;
 
 use crate::client::parse::SessionHeader;
 use crate::codec::CodecItem;
+use crate::tokio::Bind;
 use crate::{
     Error, ErrorInt, RtspMessageContext, StreamContext, StreamContextInner, TcpStreamContext,
     UdpStreamContext,
@@ -506,6 +508,7 @@ pub struct SessionOptions {
     unassigned_channel_data: UnassignedChannelDataPolicy,
     session_id: SessionIdPolicy,
     follow_redirects: bool,
+    bind: Option<String>,
 }
 
 /// Policy for handling data received on unassigned RTSP interleaved channels.
@@ -717,6 +720,11 @@ impl SessionOptions {
 
     pub fn follow_redirects(mut self, follow_redirects: bool) -> Self {
         self.follow_redirects = follow_redirects;
+        self
+    }
+
+    pub fn bind(mut self, bind: String) -> Self {
+        self.bind = Some(bind);
         self
     }
 }
@@ -1204,11 +1212,15 @@ enum SessionFlag {
 }
 
 impl RtspConnection {
-    async fn connect(url: &Url) -> Result<Self, Error> {
+    async fn connect(url: &Url, bind: Option<&str>) -> Result<Self, Error> {
         let host =
             RtspConnection::validate_url(url).map_err(|e| wrap!(ErrorInt::InvalidArgument(e)))?;
         let port = url.port().unwrap_or(554);
-        let inner = crate::tokio::Connection::connect(host, port)
+        let bind = match bind {
+            None => None,
+            Some(bind) => Some(Bind::from_str(bind)?),
+        };
+        let inner = crate::tokio::Connection::connect(host, port, bind)
             .await
             .map_err(|e| wrap!(ErrorInt::ConnectError(e)))?;
         Ok(Self {
@@ -1522,7 +1534,7 @@ impl Session<Described> {
     pub async fn describe(url: Url, options: SessionOptions) -> Result<Self, Error> {
         let mut url = url;
         loop {
-            let conn = RtspConnection::connect(&url).await?;
+            let conn = RtspConnection::connect(&url, options.bind.as_deref()).await?;
             let result = Self::describe_with_conn(conn, options.clone(), url).await;
             if !options.follow_redirects {
                 return result;
